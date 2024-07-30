40b7f19a7e0b6d9017c1f24807e45bc2fe1a7d69
carlosmacarao <carlosmacarao9@gmail.com>
2024-07-30 15:19:08 +0200

    update class

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -6,10 +6,10 @@ class Restaurante:
 restaurante_praca = Restaurante()
 restaurante_praca.nome = 'Praça'
 restaurante_praca.categoria = 'Gourmet'
-restaurante_praca.ativo = True
+
 
 restaurante_pizza = Restaurante()    
 
 restaurantes = [restaurante_praca, restaurante_pizza]
 
-print(restaurantes)
+print(vars(restaurante_praca))
